6f39c158c023f0b77f524ab15c9f4361f930f9b1
Aadi Tyagi <aadityagi1232@gmail.com>
2025-06-17 19:07:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,34 @@
-import './App.css'
-import Main from './components/Main'
-import { LayoutDashboard, Home, StickyNote, Layers, Flag, Calendar, LifeBuoy, Settings } from "lucide-react";
-import Sidebar, { SidebarItem } from "./components/SideBar"
-import SongInfo from './components/SongInfo'
+import './App.css';
+import { LayoutDashboard, StickyNote, Calendar } from "lucide-react";
+import Sidebar, { SidebarItem } from "./components/SideBar";
+import SongInfo from './components/SongInfo';
 import ExploreInfo from './components/Explore/ExploreInfo';
 import { Outlet } from 'react-router-dom';
 
-
-
-
 function App() {
-
   return (
-    
-   <>
-   <div className='flex'>
-   <div className="flex w-[23%]">
+    <div className="flex min-h-screen">
+      {/* Sidebar */}
+      <div className="flex w-[23%]">
         <Sidebar>
-          <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard"   />
-          <SidebarItem icon={<StickyNote size={20} />} text="Playlist"  />
-          <SidebarItem icon={<Calendar size={20} />} text="Liked" />
+          <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" path="/" />
+          <SidebarItem icon={<StickyNote size={20} />} text="Playlist" path="/Playlist" />
+          <SidebarItem icon={<Calendar size={20} />} text="Liked" path="/liked" />
         </Sidebar>
       </div>
-    <div className='w-[320%]'>
-    <Outlet/>
+
+      {/* Main Content */}
+      <div className="flex-grow px-4 py-6 overflow-auto">
+        <Outlet />
+      </div>
+
+      {/* Right Info Panels */}
+      <div className="w-[23%]">
+        <SongInfo />
+        <ExploreInfo />
+      </div>
     </div>
-    <div className='w-[23%]'>
-    <SongInfo/>
-    <ExploreInfo/>
-    
-    </div>
-  
-
-
-
-    </div>
-
-  
-   <div>
-    {/* <audio src="Level.mp3" controls></audio> */}
-   </div>
-   </>
-  )
+  );
 }
 
-export default App
+export default App;
